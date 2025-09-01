5715de0ee5f27d191c9857b118cd98907f4131c7
lessismore22 <charlesigbe0@gmail.com>
2025-09-01 15:33:39 +0100

    updated settings.py to allow for render hosting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ALLOWED HOSTS']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
