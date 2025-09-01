8676a80186d03ef56654dad7d6b3f2f0a423b097
lessismore22 <charlesigbe0@gmail.com>
2025-09-01 13:16:17 +0100

    allowed hosts changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ALLOWED HOSTS']
 
 
 # Application definition
