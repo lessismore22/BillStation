ca1434e1e3ddbecd9e30c41d9ab254808ae4f423
lessismore22 <charlesigbe0@gmail.com>
2025-09-03 04:28:06 +0100

    update settings.py for corsheaders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth_service',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
