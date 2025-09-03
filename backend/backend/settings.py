4e6b4cee025b8cd76f0adaf20cb8d70d548f19f7
lessismore22 <charlesigbe0@gmail.com>
2025-09-03 06:37:12 +0100

    update allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'].split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'billstation-3.onrender.com']
 
 
 # Application definition
